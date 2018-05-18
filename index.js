e589c92dc8ea1df9d7fbe4d8cfc74b65ec0a662e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-18 23:57:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="' + r.html_url + '">Visit</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
+function getRepositories() {
+  let username = document.getElementById('username').value
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/users/${username}/repos`)
+  req.send()
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
