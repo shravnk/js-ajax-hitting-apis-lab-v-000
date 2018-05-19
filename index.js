76e75b8d0246e50d790ad7b976803480e8ca71a0
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-19 00:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function displayBranches() {
 function getRepositories() {
   let username = document.getElementById('username').value
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", `https://api.github.com/users/${username}/repos`)
   req.send()
 }
