aaa3d8d8fc378ecca4e3870e6d4fe4c471db02e8
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-19 00:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function displayCommits() {
 
 function displayBranches() {
   const branches = JSON.parse(this.responseText)
-  const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.name + '</strong> /li>').join('')}</ul>`
+  const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.name + '</strong></li>').join('')}</ul>`
   document.getElementById("details").innerHTML = branchesList
 }
 
