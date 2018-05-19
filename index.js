41e403989ec7801684bf2ec673cafdc74f6a630a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-19 00:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function showRepositories(event, data) {
 function showCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
+  document.getElementById("details").innerHTML = commitsList
 }
 
 function getRepositories() {
